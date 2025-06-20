fd490fc9dd0ee86b1eeed2e01d6d95a25ec70d36
Mert Ahmet Veli <mert.av@icloud.com>
2025-06-20 15:39:56 +0300

    Add files via upload

---
+++ eReconcilition.DataAccess/Concreate/EntityFramework/EFCompanyDal.cs
@@ -0,0 +1,31 @@
+﻿using eReconcilition.Core.DataAccess.EntityFramework;
+using eReconcilition.DataAccess.Abstract;
+using eReconcilition.DataAccess.Concreate.EntityFramework.Context;
+using eReconcilition.Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eReconcilition.DataAccess.Concreate.EntityFramework
+{
+    public class EFCompanyDal : EfEntityRepositoryBase<Company, ContextDB>, ICompanyDal
+    {
+        public void UserCompanyAdd(int userId, int companyId)
+        {
+            using (var context = new ContextDB())
+            {
+                UserCompany userCompany = new UserCompany()
+                {
+                    UserId = userId,
+                    CompanyId = companyId,
+                    AddDate = DateTime.Now,
+                    IsActive = true
+                };
+                context.UserCompanies.Add(userCompany); 
+               
+            }
+        }
+    }
+}
